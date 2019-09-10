76a81d0e00fdaddceca9c72322ccaffcc4fd2e99
BiancaxToledo <bianca.dharas2@gmail.com>
2019-09-09 21:51:06 -0300

    Aula 06

---
+++ app/src/main/java/br/com/aula06/Segunda_Activity.java
@@ -0,0 +1,17 @@
+package br.com.aula06;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Segunda_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+    }
+
+
+}
